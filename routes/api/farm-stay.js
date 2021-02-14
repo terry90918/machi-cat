84ee72a3da5350287b4cc6e03e79979680b72be6
terry.chen <zxtw17985321@gmail.com>
2021-02-15 00:59:28 +0800

    chore: 移除測試 500 錯誤狀態

--- routes/api/farm-stay.js
+++ routes/api/farm-stay.js
@@ -11,9 +11,7 @@ router.get('/', (req, res, next) => {
     .catch((error) => {
       console.log(error);
     }) */
-  res
-    .status(500)
-    .json(farmStay);
+  res.json(farmStay);
 });
 
 module.exports = router;
