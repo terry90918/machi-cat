f0eb1e44cc67296cb77078079b31a9392fb039ca
terry.chen <zxtw17985321@gmail.com>
2021-02-15 00:50:05 +0800

    feat: 資料暫存成靜態資源

--- routes/api/farm-stay.js
+++ routes/api/farm-stay.js
@@ -1,15 +1,19 @@
-var axios = require('axios');
+// var axios = require('axios');
 var express = require('express');
 var router = express.Router();
+var farmStay = require('../../database/farm-stay.json')
 
 router.get('/', (req, res, next) => {
-  axios.get('https://ezgo.coa.gov.tw/API/FarmStay?lang=zh-TW')
+  /* axios.get('https://ezgo.coa.gov.tw/API/FarmStay?lang=zh-TW')
     .then((response) => {
       res.json(response.data);
     })
     .catch((error) => {
       console.log(error);
-    })
+    }) */
+  res
+    .status(500)
+    .json(farmStay);
 });
 
 module.exports = router;
